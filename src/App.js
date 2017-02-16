870a2d8ad786fed4e40c45992c7baf4cf4388f53
joyu <775154773@qq.com>
2017-02-16 19:48:27 +0800

    update

--- src/App.js
+++ src/App.js
@@ -2,7 +2,9 @@ import React, { Component } from 'react';
 import './App.css';
 import  {getJSON} from 'jquery';
 
-class Rate extends Component {
+const RATEURL = 'http://118.89.142.46:7777/';
+
+export default class Rate extends Component {
   constructor(props) {
     super(props);
     this.state={
@@ -11,14 +13,17 @@ class Rate extends Component {
         name:[],
         updateTime:''
       },
-      userInput: '1',
+      basePos: 0,
+      userInput: 1,
     };
+    
     this.getRate = this.getRate.bind(this);
-    this.handleChange = this.handleChange.bind(this);
+    this.changeValue = this.changeValue.bind(this);
+    this.changeBase = this.changeBase.bind(this);
   }
 
   getRate(){
-    getJSON('http://123.206.211.247:7777/', (data) => {
+    getJSON(RATEURL, (data) => {
       let price = [];
       let name = [];
       let tmp = data.result[0];
@@ -27,6 +32,11 @@ class Rate extends Component {
         name.push(tmp[v]['name']);
         price.push(100 / tmp[v]['bankConversionPri']);
       }
+
+      //手动添加人民币
+      name.unshift('人民币');
+      price.unshift(1);
+
       this.setState({
         rateData:{
           price: price,
@@ -41,28 +51,36 @@ class Rate extends Component {
     this.getRate();
   }
 
-  handleChange(e){
+  changeValue(e){
     this.setState({userInput: e.target.value});
+  }
+
+  changeBase(pos){
+    
+    this.setState({
+      basePos:pos
+    });
   }
 
   render(){
     return(
       <div className="rate-box">
-        <h1>货币汇率</h1>
-        <Ratetime time={this.state.rateData.updateTime} />
-        <Ratebase val={this.state.userInput} onChangeValue={this.handleChange} />
-        <Rateshow {...this.state} />
+        <RateHead time={this.state.rateData.updateTime} />
+        <Ratebase onChangeValue={this.changeValue} {...this.state}/>
+        <Rateshow onChangeBase={this.changeBase} {...this.state} />
       </div>
     );
   }
 }
 
 
-class Ratetime extends Component {
+
+class RateHead extends Component {
 
   render() {
     return (
       <div className="rate-time">
+        <h1>货币汇率</h1>
         <span>最新更新：</span>
         <span>{this.props.time}</span>
       </div>
@@ -70,27 +88,32 @@ class Ratetime extends Component {
   }
 }
 
-class Ratebase extends Component {
 
+
+class Ratebase extends Component {
+  
   render() {
     return (
       <div className="rate-base">
-        <label htmlFor="base-input">人民币：</label>
+        <label htmlFor="base-input" className="base-name">{this.props.rateData.name[this.props.basePos]}：</label>
         <input
           type="text"
           id="base-input"
           placeholder="请输入要转换数量"
-          value={this.props.val}
+          value={this.props.userInput}
           onChange={this.props.onChangeValue}
           maxLength="20"
         />
-        <span className="base-tip">{isNaN(Number(this.props.val)) ? '你...你这...你这多少钱?' : ''}</span>
+        <span className="base-tip">{isNaN(Number(this.props.userInput)) ? '你...你这...你这多少钱?' : ''}</span>
       </div>
     );
   }
 }
 
+
+
 class Rateshow extends Component {
+
 
   render() {
     return (
@@ -100,12 +123,12 @@ class Rateshow extends Component {
           return(
             <li className="rate-item clearfix" key={`ratename${i}`}>
 
-              <span className="rate-name">
+              <span className="rate-name" title="点击切换" onClick={this.props.onChangeBase.bind(null,i)}>
                 {this.props.rateData.name[i]}：
               </span>
 
               <span className="rate-number" key={`rateprice${i}`}>
-                {(v * this.props.userInput).toFixed(2)}
+                {(v * this.props.userInput / this.props.rateData.price[this.props.basePos]).toFixed(2)}
               </span>
               
             </li>
@@ -116,6 +139,3 @@ class Rateshow extends Component {
     );
   }
 }
-
-
-export default Rate;
